95c138bc609ec6ad8318768019674d2652867d5a
vadim-arinoff <carralfull@gmail.com>
2025-09-25 16:29:56 +0300

    Fixed web_form component

--- local/templates/web_form/components/bitrix/form.result.new/contact_form/template.php
+++ local/templates/web_form/components/bitrix/form.result.new/contact_form/template.php
@@ -17,98 +17,117 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
     </div>
 
     <?if ($arResult["isFormErrors"] == "Y"):?>
-        <div class="contact-form__error-summary" style="color:red; border: 1px solid red; padding: 10px; margin-bottom:15px;">
+        <div class="contact-form__error-summary">
             <?=$arResult["FORM_ERRORS_TEXT"];?>
         </div>
     <?endif;?>
 
-    <form class="contact-form__form" name="<?=$arResult["arForm"]["SID"]?>" action="" method="POST">
-        <?=bitrix_sessid_post()?>
+    <?=$arResult["FORM_HEADER"]?>
 
         <div class="contact-form__form-inputs">
             <?
             $sid = "USER_NAME";
             $arQuestion = $arResult["QUESTIONS"][$sid];
-            $inputName = $arQuestion['STRUCTURE'][0]['FIELD_NAME']; // Правильное имя из массива
-            $inputValue = htmlspecialcharsbx($arResult['arrVALUES'][$inputName]);
+            if($arQuestion){
+                $inputName = 'form_' . $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $arQuestion['STRUCTURE'][0]['ID'];
+                $inputValue = isset($arResult['arrVALUES'][$inputName]) ? htmlspecialcharsbx($arResult['arrVALUES'][$inputName]) : '';
             ?>
             <div class="input contact-form__input">
                 <label class="input__label" for="medicine_name">
                     <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if($arQuestion["REQUIRED"] == "Y"):?>*<?endif;?></div>
                     <input class="input__input" type="text" id="medicine_name" name="<?=$inputName?>" value="<?=$inputValue?>" <?if($arQuestion["REQUIRED"] == "Y"):?>required=""<?endif;?>>
-                    <div class="input__notification"><?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$sid]);?></div>
+                    <?if(!empty($arResult["FORM_ERRORS"][$sid])):?>
+                    <div class="input__notification"><?=$arResult["FORM_ERRORS"][$sid];?></div><?endif;?>
                 </label>
             </div>
-
             <?
+            }
+
             $sid = "USER_COMPANY";
             $arQuestion = $arResult["QUESTIONS"][$sid];
-            $inputName = $arQuestion['STRUCTURE'][0]['FIELD_NAME'];
-            $inputValue = htmlspecialcharsbx($arResult['arrVALUES'][$inputName]);
+            if($arQuestion){
+                $inputName = 'form_' . $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $arQuestion['STRUCTURE'][0]['ID'];
+                $inputValue = isset($arResult['arrVALUES'][$inputName]) ? htmlspecialcharsbx($arResult['arrVALUES'][$inputName]) : '';
             ?>
             <div class="input contact-form__input">
                 <label class="input__label" for="medicine_company">
                     <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if($arQuestion["REQUIRED"] == "Y"):?>*<?endif;?></div>
                     <input class="input__input" type="text" id="medicine_company" name="<?=$inputName?>" value="<?=$inputValue?>" <?if($arQuestion["REQUIRED"] == "Y"):?>required=""<?endif;?>>
-                    <div class="input__notification"><?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$sid]);?></div>
+                    <?if(!empty($arResult["FORM_ERRORS"][$sid])):?>
+                    <div class="input__notification"><?=$arResult["FORM_ERRORS"][$sid];?></div><?endif;?>
                 </label>
             </div>
-
             <?
+            }
+
             $sid = "USER_EMAIL";
             $arQuestion = $arResult["QUESTIONS"][$sid];
-            $inputName = $arQuestion['STRUCTURE'][0]['FIELD_NAME'];
-            $inputValue = htmlspecialcharsbx($arResult['arrVALUES'][$inputName]);
+            if ($arQuestion) {
+                $inputName = 'form_' . $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $arQuestion['STRUCTURE'][0]['ID'];
+                $inputValue = isset($arResult['arrVALUES'][$inputName]) ? htmlspecialcharsbx($arResult['arrVALUES'][$inputName]) : '';
             ?>
             <div class="input contact-form__input">
                 <label class="input__label" for="medicine_email">
                     <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if($arQuestion["REQUIRED"] == "Y"):?>*<?endif;?></div>
                     <input class="input__input" type="email" id="medicine_email" name="<?=$inputName?>" value="<?=$inputValue?>" <?if($arQuestion["REQUIRED"] == "Y"):?>required=""<?endif;?>>
-                    <div class="input__notification"><?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$sid]);?></div>
+                    <?if(!empty($arResult["FORM_ERRORS"][$sid])):?>
+                    <div class="input__notification"><?=$arResult["FORM_ERRORS"][$sid];?></div><?endif;?>
                 </label>
             </div>
-
             <?
+            }
+
             $sid = "USER_PHONE";
             $arQuestion = $arResult["QUESTIONS"][$sid];
-            $inputName = $arQuestion['STRUCTURE'][0]['FIELD_NAME'];
-            $inputValue = htmlspecialcharsbx($arResult['arrVALUES'][$inputName]);
+            if ($arQuestion) {
+                $inputName = 'form_' . $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $arQuestion['STRUCTURE'][0]['ID'];
+                $inputValue = isset($arResult['arrVALUES'][$inputName]) ? htmlspecialcharsbx($arResult['arrVALUES'][$inputName]) : '';
             ?>
             <div class="input contact-form__input">
                 <label class="input__label" for="medicine_phone">
                     <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if($arQuestion["REQUIRED"] == "Y"):?>*<?endif;?></div>
                     <input class="input__input" type="tel" id="medicine_phone" name="<?=$inputName?>" value="<?=$inputValue?>" <?if($arQuestion["REQUIRED"] == "Y"):?>required=""<?endif;?>
                            data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" maxlength="12" x-autocompletetype="phone-full">
-                    <div class="input__notification"><?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$sid]);?></div>
+                    <?if(!empty($arResult["FORM_ERRORS"][$sid])):?>
+                    <div class="input__notification"><?=$arResult["FORM_ERRORS"][$sid];?></div><?endif;?>
                 </label>
             </div>
+            <?
+            }
+            ?>
         </div>
 
         <div class="contact-form__form-message">
             <?
             $sid = "MESSAGE";
             $arQuestion = $arResult["QUESTIONS"][$sid];
-            $inputName = $arQuestion['STRUCTURE'][0]['FIELD_NAME'];
-            $inputValue = htmlspecialcharsbx($arResult['arrVALUES'][$inputName]);
+            if ($arQuestion) {
+                $inputName = 'form_' . $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $arQuestion['STRUCTURE'][0]['ID'];
+                $inputValue = isset($arResult['arrVALUES'][$inputName]) ? htmlspecialcharsbx($arResult['arrVALUES'][$inputName]) : '';
             ?>
             <div class="input">
                 <label class="input__label" for="medicine_message">
                     <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if($arQuestion["REQUIRED"] == "Y"):?>*<?endif;?></div>
                     <textarea class="input__input" id="medicine_message" name="<?=$inputName?>" <?if($arQuestion["REQUIRED"] == "Y"):?>required=""<?endif;?>><?=$inputValue?></textarea>
-                    <div class="input__notification"><?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$sid]);?></div>
+                    <?if(!empty($arResult["FORM_ERRORS"][$sid])):?>
+                    <div class="input__notification"><?=$arResult["FORM_ERRORS"][$sid];?></div><?endif;?>
                 </label>
             </div>
+            <?
+            }
+            ?>
         </div>
 
         <div class="contact-form__bottom">
-    <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.</div>
+            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.</div>
 
-    <input type="hidden" name="web_form_submit" value="Y" />
-    
-    <button class="form-button contact-form__bottom-button" type="submit">
-        <div class="form-button__title">Оставить заявку</div>
-    </button>
-</div>
-    </form>
+            <button class="form-button contact-form__bottom-button" type="submit" name="web_form_submit" value="Y">
+                <div class="form-button__title">
+                    <?=htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? "Оставить заявку" : $arResult["arForm"]["BUTTON"]);?>
+                </div>
+            </button>
+        </div>
+
+    <?=$arResult["FORM_FOOTER"]?>
 </div>
 <?endif;?>
